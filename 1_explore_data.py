eb2016367eddf651498bbc10e269ead1b8a56820
skyingsky@gmail.com <skyingsky@gmail.com>
2020-09-15 21:34:55 -0700

    Clean

--- 1_explore_data.py
+++ 1_explore_data.py
@@ -130,14 +130,99 @@ def convert_sex():
     return train_df, test_df, combine
 
 
+def guess_age():
+    train_df, test_df, combine = convert_sex()
+    guess_ages = np.zeros((2,3))
+    for dataset in combine:
+        for i in range(0, 2):
+            for j in range(0, 3):
+                guess_df = dataset[(dataset['Sex'] == i) & (dataset['Pclass'] == j+1)]['Age'].dropna()
+                # age_mean = guess_df.mean()
+                # age_std = guess_df.std()
+                # age_guess = rnd.uniform(age_mean - age_std, age_mean + age_std)
+                guess_ages[i,j]=guess_df.median()
+                # Convert random age float to nearest .5 age
+                # guess_ages[i,j] = int( age_guess/0.5 + 0.5 ) * 0.5
+        # print(guess_df)        
+        for i in range(0, 2):
+            for j in range(0, 3):
+                dataset.loc[ (dataset.Age.isnull()) & (dataset.Sex == i) & (dataset.Pclass == j+1),'Age'] = guess_ages[i,j]
+        dataset['Age'] = dataset['Age'].astype(int)
+    # print(train_df.head())
+    return train_df, test_df, combine
+
+def convert_age():
+    train_df, test_df, combine = guess_age()
+    train_df['AgeBand'] = pd.cut(train_df['Age'], 5)
+    train_df[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=False).mean().sort_values(by='AgeBand', ascending=True)
+    for dataset in combine:    
+        dataset.loc[ dataset['Age'] <= 16, 'Age'] = 0
+        dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+        dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+        dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+        dataset.loc[ dataset['Age'] > 64, 'Age']
+    train_df = train_df.drop(['AgeBand'], axis=1)
+    combine = [train_df, test_df]
+    # print(train_df.head())
+    return train_df, test_df, combine
 
 
+def convert_family_size():
+    train_df, test_df, combine = convert_age()
+    # Add family size
+    for dataset in combine:
+        dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+    # print(train_df[['FamilySize', 'Survived']].groupby(['FamilySize'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+    # Add IsAlone
+    for dataset in combine:
+        dataset['IsAlone'] = 0
+        dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
+    # print(train_df[['IsAlone', 'Survived']].groupby(['IsAlone'], as_index=False).mean())
+    # Drop Parch Sibsp and Family Size
+    train_df = train_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
+    test_df = test_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
+    combine = [train_df, test_df]
+    # print(train_df.head())
+    return train_df, test_df, combine
 
 
+def add_AgePclass():
+    train_df, test_df, combine = convert_family_size()
+    for dataset in combine:
+        dataset['Age*Class'] = dataset.Age * dataset.Pclass
+    # print(train_df.loc[:, ['Age*Class', 'Age', 'Pclass']].head(10))
+    return train_df, test_df, combine
 
 
+def convert_Embarked():
+    train_df, test_df, combine = add_AgePclass()
+    freq_port = train_df.Embarked.dropna().mode()[0]
+    for dataset in combine:
+        dataset['Embarked'] = dataset['Embarked'].fillna(freq_port)
+    for dataset in combine:
+        dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+    # print(train_df.head())
+    return train_df, test_df, combine
+
+def convert_fare():
+    train_df, test_df, combine = convert_Embarked()
+    test_df['Fare'].fillna(test_df['Fare'].dropna().median(), inplace=True)
+    train_df['FareBand'] = pd.qcut(train_df['Fare'], 4)
+    # train_df[['FareBand', 'Survived']].groupby(['FareBand'], as_index=False).mean().sort_values(by='FareBand', ascending=True)
+    for dataset in combine:
+        dataset.loc[ dataset['Fare'] <= 7.91, 'Fare'] = 0
+        dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+        dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare']   = 2
+        dataset.loc[ dataset['Fare'] > 31, 'Fare'] = 3
+        dataset['Fare'] = dataset['Fare'].astype(int)
+    train_df = train_df.drop(['FareBand'], axis=1)
+    combine = [train_df, test_df]
+    print(train_df.head(10))
+    print(test_df.head(10))
+    return train_df, test_df, combine
 
 
+convert_fare()
 
 
 # Note:
